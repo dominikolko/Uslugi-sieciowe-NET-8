71df790d7ee583f9318122ee9d674f31e6ebe950
Dominik <olkodominik@gmail.com>
2026-05-25 19:49:58 +0200

    Kontrolery - Zadanie 1

---
+++ 04 Kontrolery WebAPI/exercises/Zadanie 1/Controllers/CitiesController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WeatherAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CitiesController : ControllerBase
+{
+    private readonly AppDbContext _db;
+
+    public CitiesController(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<City>>> GetAll()
+    {
+        return await _db.Cities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<City>> GetById(int id)
+    {
+        var city = await _db.Cities.FindAsync(id);
+
+        if (city == null)
+            return NotFound();
+
+        return city;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<City>> Create(string cityName)
+    {
+        var city = new City { Name = cityName };
+
+        _db.Cities.Add(city);
+        await _db.SaveChangesAsync();
+
+        return Ok(city);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Update(int id, string cityName)
+    {
+        var city = await _db.Cities.FindAsync(id);
+
+        if (city == null)
+            return NotFound();
+
+        city.Name = cityName;
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        var city = await _db.Cities.FindAsync(id);
+
+        if (city == null)
+            return NotFound();
+
+        _db.Cities.Remove(city);
+        await _db.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
