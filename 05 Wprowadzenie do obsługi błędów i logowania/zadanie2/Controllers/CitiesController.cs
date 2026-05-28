307411188623925fd845521016929f95823da217
Dominik <olkodominik@gmail.com>
2026-05-28 18:44:12 +0200

    Dodano logowanie Serilog

---
+++ 05 Wprowadzenie do obsługi błędów i logowania/zadanie2/Controllers/CitiesController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zadanie1Swagger.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        private static List<string> cities = new List<string>();
+
+        private readonly ILogger<CitiesController> _logger;
+
+        public CitiesController(ILogger<CitiesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            _logger.LogInformation("Pobrano listę miast. Liczba miast: {CitiesCount}", cities.Count);
+
+            return Ok(cities);
+        }
+
+        [HttpPost]
+        public IActionResult AddCity([FromBody] CityRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Name))
+            {
+                _logger.LogWarning("Próba dodania miasta z pustą nazwą.");
+
+                return BadRequest("Nazwa miasta nie może być pusta.");
+            }
+
+            cities.Add(request.Name);
+
+            _logger.LogInformation("Dodano nowe miasto: {CityName}", request.Name);
+
+            return Ok(new
+            {
+                message = "Miasto dodane poprawnie",
+                addedCity = request.Name
+            });
+        }
+    }
+
+    public class CityRequest
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ 05 Wprowadzenie do obsługi błędów i logowania/zadanie2/Program.cs
@@ -0,0 +1,36 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration
+    .ReadFrom.Configuration(hostingContext.Configuration));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    // app.UseDeveloperExceptionPage();
+    app.UseExceptionHandler("/api/error");
+}
+else
+{
+    app.UseExceptionHandler("/api/error");
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
