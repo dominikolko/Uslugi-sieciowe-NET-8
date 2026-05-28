a328de26571e91fe1d462f4977d34d422571dc8d
Dominik <olkodominik@gmail.com>
2026-05-28 17:21:13 +0200

    Dodano zadanie 1 obsluga bledow

---
+++ 05 Wprowadzenie do obsługi błędów i logowania/zadanie1/Controllers/CitiesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Zadanie1Swagger.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CitiesController : ControllerBase
+    {
+        private static List<string> cities = new List<string>();
+
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(cities);
+        }
+
+        [HttpPost]
+        public IActionResult AddCity([FromBody] CityRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.Name))
+            {
+                return BadRequest("Nazwa miasta nie może być pusta.");
+            }
+
+            cities.Add(request.Name);
+
+            return Ok(new
+            {
+                message = "Miasto dodane poprawnie",
+                addedCity = request.Name
+            });
+        }
+    }
+
+    public class CityRequest
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
