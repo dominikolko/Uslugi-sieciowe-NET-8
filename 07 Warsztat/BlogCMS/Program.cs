605a297b49bad78f132ea177f41449b29bb84c14
Dominik <olkodominik@gmail.com>
2026-06-13 18:46:08 +0200

    BlogCMS CRUD API

---
+++ 07 Warsztat/BlogCMS/Program.cs
@@ -0,0 +1,46 @@
+using BlogCMS.Data;
+using Microsoft.EntityFrameworkCore;
+using BlogCMS.Repositories;
+
+namespace BlogCMS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddDbContext<BlogDbContext>(options =>
+            options.UseSqlServer(
+            builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(EfCoreRepository<>));
+
+            builder.Services.AddControllers();
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
