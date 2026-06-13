dab9f4c93f23ed8aeac72376df8f1bfb24aaef79
Dominik <olkodominik@gmail.com>
2026-06-13 18:08:11 +0200

    CRUD z bazą danych

---
+++ 06 CRUD z bazą danych/TravelQuotesApi/Controllers/QuotesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelQuotesApi.Data;
+using TravelQuotesApi.Models;
+
+namespace TravelQuotesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuotesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Quote>>> GetQuotes()
+        {
+            return await _context.Quotes.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Quote>> PostQuote(Quote quote)
+        {
+            _context.Quotes.Add(quote);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetQuotes), new { id = quote.Id }, quote);
+        }
+    }
+}
